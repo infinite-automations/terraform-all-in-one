241f5c7e749970d1af752807f39ad0a288ff8c23
Marius Boden <mboden@xpirit.com>
2023-10-05 11:55:11 +0000

    test(terraform): set version constraints for test project

--- test/terraform/main.tf
+++ test/terraform/main.tf
@@ -1,4 +1,14 @@
 # Define the provider
+terraform {
+  required_version = "~>1.0"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
 provider "local" {}
 
 variable "filename" {
